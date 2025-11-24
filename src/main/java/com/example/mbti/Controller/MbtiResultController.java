672fda5d80d2cdf2d4df3c292d51d51b58adfde9
202310957 <snu11025@naver.com>
2025-11-24 20:01:14 +0900

    feat: 최종

--- src/main/java/com/example/mbti/Controller/MbtiResultController.java
+++ src/main/java/com/example/mbti/Controller/MbtiResultController.java
@@ -5,11 +5,8 @@ import com.example.mbti.DTO.MbtiResultResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import com.example.mbti.Service.ResultCalcService;
-import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
