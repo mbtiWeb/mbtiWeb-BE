e245a9addbe62e1eaaadcc091aa448af429a900e
202310957 <snu11025@naver.com>
2026-01-02 21:36:56 +0900

    fix: HTTPS 프록시 환경의 Mixed Content 에러 해결

---
+++ src/main/java/com/example/mbti/Service/GptService.java
@@ -0,0 +1,9 @@
+package com.example.mbti.Service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor  // 생성자 주입
+public class GptService {
+}
