4a588575304ca77edb1c269e559b424d40961dd2
202310957 <snu11025@naver.com>
2026-01-02 22:13:08 +0900

    fix: CORS 문제 해결

--- src/main/java/com/example/mbti/Config/Config.java
+++ src/main/java/com/example/mbti/Config/Config.java
@@ -1,22 +1,18 @@
 package com.example.mbti.Config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
+@OpenAPIDefinition(
+        servers = {
+                @Server(
+                        url = "https://mbti-api-java.soyyoda.com",
+                        description = "Production Server (HTTPS)"
+                )
+        }
+)
 public class Config {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        // Swagger UI에서 모든 API 호출 시 이 주소를 기반으로 요청을 보냅니다.
-        Server server = new Server();
-        server.setUrl("https://mbti-api-java.soyyoda.com");
-        server.setDescription("Production Server (HTTPS)");
-
-        return new OpenAPI().servers(List.of(server));
-    }
 }
